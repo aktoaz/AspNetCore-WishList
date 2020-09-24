89fb5a3ce5013b777c95dc50535c09404469f7bc
Paul Johnson <paulandshawna@gmail.com>
2020-09-24 13:16:46 -0700

    4

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -36,11 +36,8 @@ namespace WishList.Controllers
             return RedirectToAction("Index");
         }
 
-        //[HttpPost]
         public IActionResult Delete(int Id)
         {
-            //var myitem = _context.Items.Find(Id);
-            //var myitem = _context.Items.FirstOrDefault(e => e.Id == Id);
             var item = _context.Items.FirstOrDefault(e => e.Id == Id);
             _context.Items.Remove(item);
             _context.SaveChanges();
